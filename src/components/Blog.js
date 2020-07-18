9eb3d31171866c3bac06a00644ae159d3dfce1db
EileenJuergens <juergens.eileen@gmail.com>
2020-07-18 18:00:40 +0200

    feat: finish part 5.7 - bloglist frontend

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div className='single-blog'>
-    <p>{blog.title}</p>
-    <p>{blog.author}</p>
-    <p>{blog.url}</p>
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [detailsAreShown, setDetailsAreShown] = useState(false);
+
+  const toggleView = () => {
+    setDetailsAreShown(!detailsAreShown)
+  }
+
+  return (
+    <div className='single-blog'>
+      <p><b>{blog.title}</b> {blog.author} <button
+        onClick={toggleView}>{detailsAreShown ? 'hide' : 'view'}</button></p>
+      {detailsAreShown && (
+        <>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.user.name}</p>
+        </>
+      )}
+    </div>
+  )
+}
+
 
 export default Blog
