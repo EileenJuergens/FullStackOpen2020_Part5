05c1817728347201ae64c9a052fd4320f737a694
EileenJuergens <juergens.eileen@gmail.com>
2020-09-11 13:26:13 +0200

    feat: finish part 5.18 - bloglist frontend

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,28 +1,40 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:3000')
   })
 
-  it('front page can be opened', function() {
+  it('front page can be opened', function () {
     cy.contains('LOGIN')
   })
 
-  it('Login form is shown', function() {
+  it('login form is shown', function () {
     cy.contains('Username')
     cy.contains('Password')
   })
 
-  it('login form can be opened', function() {
+  it('login form can be opened', function () {
     cy.contains('LOGIN').click()
   })
+})
 
-  // it('user can login', function () {
-  //   cy.contains('LOGIN').click()
+describe('Login', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:3000')
+  })
 
-  //   cy.get('#username').type('Cassy')
-  //   cy.get('#password').type('test123')
-  //   cy.get('#login-button').click()
+  it('fails with wrong credentials', function() {
+    cy.get('#username').type('Unknown')
+    cy.get('#password').type('test')
+    cy.get('#login-button').click()
 
-  //   cy.contains('Cassandra Sloan is logged in')
-  // })
+    cy.contains('Wrong credentials')
+  })
+
+  it('succeeds with correct credentials', function () {
+    cy.get('#username').type('Cassy')
+    cy.get('#password').type('test123')
+    cy.get('#login-button').click()
+
+    cy.contains('Cassandra Sloan is logged in')
+  })
 })
